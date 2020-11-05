d5c99f71d9ed6bf453340eb24111ffae0f48f236
Larannrf@gmail.com <54900840+LaranQ@users.noreply.github.com>
2020-11-06 01:49:15 +0300

    Лабораторная работа №2 готова. Халецкий В.В. слушатель группы DEV-J110

--- src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
@@ -8,7 +8,42 @@ package ru.avalon.java.dev.j10.labs.shapes;
  *
  * @see <a href="https://ru.wikipedia.org/wiki/%D0%9E%D0%BA%D1%80%D1%83%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C">Окружность</a>
  */
-public class Circle {
+public class Circle implements Ellipse{
+    
+    float xMax,yMax, x2,y2; // центр окружности, вводится в первую очередь и любая точка на ней
+    float r; // радиус
+
+    public Circle(float x1, float y1,float x2, float y2) {
+        xMax=x1;
+        yMax=y1;
+        this.x2=x2;
+        this.y2=y2;
+        
+        r = Shape.lengthSide(x1,y1,x2,y2);
+    }
+    
+    public Circle(float x1, float y1) { //Если нет точек поверхности - это точка.
+        xMax=x1;
+        yMax=y1;
+        
+        r = 0; // Превратит длину и площадь окружности в ноль.
+    }
+    
+    public float getLength(){
+        return 2*pi*r;
+    };
+    
+    public float getArea(){
+        return pi*r*r;
+    };
+    
+    public float getX(){
+        return xMax;
+    }
+    
+    public float getY(){
+        return yMax;
+    }
 
     /*
      * TODO: Реализовать класс 'Circle'
